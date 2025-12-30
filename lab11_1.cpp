92dbfa3c8c245b3fc46cfecc17271d5d8f68626a
Arraya <markillerth0000@gmail.com>
2025-12-30 21:25:54 +0700

    DONE

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,21 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    cin.get();
+    cin.get();
+    cin.get();
+
+    string grade[] = {"A","B+","B","C+","C","D+","D","F","W"};
+
+    srand(time(0));
+    int Rdgrade = rand()%9;
+
+    cout << "You will get " << grade[Rdgrade] << " in this 261102.";
+
+}
